83199ef0f0b3e112997bc12d7add531b11d2fe0c
Kajal Singh <141616877+KajalSingh18@users.noreply.github.com>
2024-06-28 18:29:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 // let's tackle cors
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://mernwebsite-zyvk.onrender.com",
     // origin: 'https://your-netlify-site.netlify.app',
     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
